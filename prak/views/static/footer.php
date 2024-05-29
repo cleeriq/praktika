4964523228e61380306bac9702d436ebd4cacb20
cleeriq <153808627+cleeriq@users.noreply.github.com>
2024-05-30 00:36:42 +0700

    Delete prak directory

--- prak/views/static/footer.php
+++
@@ -1,5 +0,0 @@
-    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-    <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/views/static/js/<?= $title ?>.js" language="JavaScript" type="text/javascript"></script>
-</body>
-</html>
